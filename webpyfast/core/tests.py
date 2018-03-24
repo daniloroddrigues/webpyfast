047596e18bb0fd9387496c31e6f08924547637e6
Danilo Rodrigues <danilo.roddrigues@gmail.com>
2018-03-24 01:33:51 -0300

    Projeto Inicial

---
+++ webpyfast/core/tests.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+
+    def test_get(self):
+        """Deve retorna status code 200"""
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        """Deve retornar o template """
+        self.assertTemplateUsed(self.resp, 'core/index.html')
+
+    def test_html(self):
+        """Deve conter na home esses html, """
+        contains = (
+            ('id="company"', 1),
+            ('id="services"', 1),
+            ('id="clients"', 1),
+            ('id="blog"', 1),
+            ('id="contact"', 1),
+        )
+
+        with self.subTest():
+            for contain, index in contains:
+                self.assertContains(self.resp, contain, index)
